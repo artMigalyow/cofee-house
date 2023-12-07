9a5552539586f14866045d2b8e408bf2f36593da
david-anav <art_migalev@yahoo.com>
2023-12-07 20:40:55 +0300

    feat: when clicked burger_menu_ links

--- coffe-house/index.js
+++ coffe-house/index.js
@@ -5,38 +5,55 @@ console.log('Checking validation of pages : +18\n The layout matches the desing:
 const BTN_BURGER = document.querySelector('.btn-burger');
 const MAIN = document.querySelector('.main');
 const BURGER_MENU = document.querySelector('.navigation');
+const BR_MENU_LINKS = document.querySelector('.list')
+const BTN_MENU_LINK = document.querySelector('.menu-link')
 window.onload = function () {
     burgerClick();
+    clickedMenuLinks();
+
 }
 
 
 //!     FUNCTIONS
 const burgerClick =() => {
-    BTN_BURGER.addEventListener('click', (event) => {
+    BTN_BURGER.addEventListener('click', (e) => {
         BTN_BURGER.classList.toggle('btn-burger-active');
         BURGER_MENU.classList.toggle('openBurgerMenu');
-
-
-
-
-
-
-        removeClassToClick(BTN_BURGER,'btn-burger-active');
-        // removeClassToClick(BURGER_MENU,'openBurgerMenu');
-
-
+        closeBurgerMenu(BURGER_MENU,"openBurgerMenu");
+        // removeClassToClick(BTN_BURGER,'btn-burger-active');        // removeClassToClick(BURGER_MENU,'openBurgerMenu');
     })
 
 
 }
 const removeClassToClick = (btn,clas) => {         //*close btn-burger
+
     document.addEventListener('click', (e) => {
-        const withBtn = e.composedPath().includes(btn);
+        const withBtn = e.composedPath().includes();
         if(!withBtn) {
             btn.classList.remove(clas);
         }
     })
 
+}
+const closeBurgerMenu = (menu,clas) => {
+    document.addEventListener('click', (e) => {
+        const withMenu = e.composedPath().includes(document.querySelectorAll('nav-list'));
+        if(withMenu){
+            menu.classList.toggle(clas);
+        }
+    })
+}
+const clickedMenuLinks = () => {    //*when clicked burger_menu_ links
+    const childArr = document.querySelectorAll('.list');
+    document.querySelector('.nav-list').addEventListener('click', (e) => {
+        childArr.forEach(el => {
+            let withTarget = e.composedPath().includes(el)
+            if (withTarget || e.composedPath().includes(BTN_MENU_LINK)) {
+                BURGER_MENU.classList.toggle('openBurgerMenu');
+                removeClassToClick(BTN_BURGER,'btn-burger-active');
+            }
+        })
+        })
 }
 
 
