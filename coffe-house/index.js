2c6d752121d4ee24ff3b80cd3144bd05dd35fbd0
david-anav <art_migalev@yahoo.com>
2023-12-11 21:46:32 +0300

    feat: pigin move

--- coffe-house/index.js
+++ coffe-house/index.js
@@ -1,13 +1,24 @@
 console.log('Checking validation of pages : +18\n The layout matches the desing: +40\n CSS Requirements: +10\n Interactivity: +32\n ALL Score: 100 points')
 
+//! IMPORT
+// import {dataFavoriteCoffee} from './data/dataSlide.js';
+// console.log(dataFavoriteCoffee[1].name)
 
 //! VARIABLES
 const BTN_BURGER = document.querySelector('.btn-burger');
 const MAIN = document.querySelector('.main');
 const BURGER_MENU = document.querySelector('.navigation');
-const BR_MENU_LINKS = document.querySelector('.list')
-const BTN_MENU_LINK = document.querySelector('.header__menu')
+const BR_MENU_LINKS = document.querySelector('.list');
+const BTN_MENU_LINK = document.querySelector('.header__menu');
+const btnPagin = document.querySelectorAll('.pagin__btn');
+
+
+
 window.onload = function () {
+    paginBtnMove();
+    setInterval(() => {
+        paginBtnMove()
+    }, 15000);
     burgerClick();
     clickedMenuLinks();
 
@@ -61,11 +72,24 @@ const clickedMenuLinks = () => {    //*when clicked burger_menu_ links
         })
 }
 
-BURGER_MENU.addEventListener('click', (event) => {
-    let target = event.target;
-    switch(target.id){
-        case 'list':
-        console.log('hi')
-    }
-})
+const paginBtnMove = () => {
+
+    setTimeout(() => {
+        btnPagin[2].classList.remove('animBtn');
+        btnPagin[0].classList.add('animBtn');
+    }, 0);
+    setTimeout(() => {
+        btnPagin[0].classList.remove('animBtn');
+        btnPagin[1].classList.add('animBtn');
+    }, 5000);
+    setTimeout(() => {
+        btnPagin[1].classList.remove('animBtn');
+        btnPagin[2].classList.add('animBtn');
+    }, 10000);
+
+
+
+
+
+}
 
