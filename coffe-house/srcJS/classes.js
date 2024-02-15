f7ebdde2c1f671637edefe71b2edc7b526d51a31
david-anav <art_migalev@yahoo.com>
2024-02-15 22:49:26 +0300

    feat: popup

--- coffe-house/srcJS/classes.js
+++ coffe-house/srcJS/classes.js
@@ -1,3 +1,45 @@
-class Block {
-  
+export class Block {
+  constructor(tag, clsName, parent, content) {
+    this.tag = tag;
+    this.clsName = clsName;
+    this.parent = parent;
+    if (content) {
+      this.content = content;
+    }
+  }
+  getBlock() {
+    const div = document.createElement(this.tag);
+    div.className = this.clsName;
+    if (typeof this.content === "string") {
+      div.textContent = this.content;
+    }
+    if (typeof this.parent === "string") {
+      document.querySelector(`.${this.parent}`).appendChild(div);
+    } else {
+      document.body.appendChild(div);
+    }
+    return div;
+  }
+}
+
+export class Popup extends Block {
+  constructor(tag, clsName) {
+    super(tag, clsName, parent);
+    this.tag;
+    this.clsName;
+  }
+}
+
+export class BtnBlock extends Block {
+  constructor(tag, clsName, content) {
+    super(tag, clsName, parent, content);
+
+  }
+}
+export class ImgBlock extends Block {
+  constructor(tag, clsName,parent,src) {
+    super(tag, clsName, parent);
+    
+    this.src = src;
+  }
 }
--- coffe-house/srcJS/index.js
+++ coffe-house/srcJS/index.js
@@ -1,1 +1,12 @@
-console.log('Hello world')
+import { Popup } from "./classes.js";
+import { popup } from "./data.js";
+
+// console.log(new Popup("dialog", "popup"))
+popup.forEach((el) => {
+  console.log(el)
+
+  el.getBlock()
+})
+const popupR = new Popup("dialog", "popup" );
+popupR.getBlock()
+// popup.forEach()
