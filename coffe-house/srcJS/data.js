9e0399df363b1fbbc6114d3ef87ebe0de7e7b362
david-anav <art_migalev@yahoo.com>
2024-04-17 22:05:12 +0300

    del coffee-houseJS

--- coffe-house/srcJS/data.js
+++
@@ -1,6 +0,0 @@
-import { Block, BtnBlock } from "./classes.js";
-
-export const modal = [
-  new BtnBlock("div", "blck-btn-sz ", "block_size", 3, "btn-sz"),
-  new BtnBlock("div", "blck-btn-ad ", "block_additeves", 3, "btn-add"),
-];
